f1acf86b2f222604e66404ab332e356169510ed8
Sizoran <77049011+Sizoran@users.noreply.github.com>
2022-03-20 11:40:42 +0300

    Lesson 4 (#4)
    * Lesson 4.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
+    "authnapp",
 ]
+
+# Auth model
+AUTH_USER_MODEL = "authnapp.ShopUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -129,3 +133,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
